3e0837e19da672ee5cfc2b8b7de213225331c07f
salvatierra1 <victordanielsalvatierra6@gmail.com>
2021-09-10 11:00:11 -0300

    Proyecto nuevo

---
+++ src/salvatierra_victor_tpo1/Salvatierra_Victor_tpo1.java
@@ -0,0 +1,20 @@
+
+package salvatierra_victor_tpo1;
+
+import IGU.Principal;
+
+
+
+
+public class Salvatierra_Victor_tpo1 {
+
+   
+    public static void main(String[] args) {
+        Principal agenda = new Principal();
+        agenda.setVisible(true);
+       agenda.setLocationRelativeTo(null);
+       
+    }
+
+    
+}
